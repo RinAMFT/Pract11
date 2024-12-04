6e036a95dc5f22f39e48a8cad6fb487a8b176397
RinAMFT <mucurkaarina@gmail.com>
2024-12-04 09:46:36 +0200

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Создать метод, который будет вычислять площадь круга и длину окружности. Вызывающий программный код должен передавать в качестве аргумента методу значение радиуса и печатать значение радиуса, площади круга и длины окружности.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int r = Input("r=");
+            double p =1,  s = 1;  
+            Func(r, ref p, ref s);
+            Console.WriteLine($"радиус ={r}\t длина окр={p}\t площадь={s}");
+            Console.ReadKey();
+
+        }
+        static void Func(int r, ref double p, ref double s)
+        {
+            p = 2*Math.PI*r;
+            s = Math.PI*Math.Pow(r, 2);
+        }
+        static int Input(string messege)
+        {
+
+            Console.Write(messege);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ConsoleApp14/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp14")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a5865395-13c2-452f-895d-1b9a01bbb114")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
